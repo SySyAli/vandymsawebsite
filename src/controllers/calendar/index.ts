bef7704142703365223f93a0d93b2b443e27cd9b
SySyAli <syedansabali786@gmail.com>
2022-12-23 22:47:32 -0500

    cal start

---
+++ src/controllers/calendar/index.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from "express"
+import express from "express"
+import {google} from "googleapis"
+
+
+//CRUD Operations for the Calendar Information
+
+// get events and them in a json form using google calendar api
+async function getCalendarEvents(req: Request, res: Response){
+    try {
+        //
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
+
+
+
