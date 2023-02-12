ded49dee4bb660327e891a5c7f53b031fc709e95
SySyAli <syedansabali786@gmail.com>
2023-02-12 00:55:17 -0600

    about us

--- my-project/app/aboutUs/page.tsx
+++ my-project/app/aboutUs/page.tsx
@@ -1,25 +1,88 @@
 export default async function aboutUs() {
 	return (
-		<div className="flex flex-col gap-4">
+		<div className="flex flex-col gap-4 text-center">
+			<div>
+				<p>
+					Welcome to the Vanderbilt Muslim Student Association (Vanderbilt MSA)
+					website! Our organization is dedicated to serving the Muslim student
+					community at Vanderbilt University and promoting understanding,
+					respect, and friendship between people of all religions and cultures.
+				</p>
+			</div>
 			<div id="msahistory" className="">
 				<div>
-                    
-                </div>
+					<h1>MSA History</h1>
+					<p>
+						The Vanderbilt MSA was established in the early 2000s as a
+						student-led initiative to create a welcoming and supportive space
+						for Muslim students at Vanderbilt. Over the years, the Vanderbilt
+						MSA has grown to become one of the most active and vibrant student
+						organizations on campus, offering a wide range of programs, events,
+						and services to its members. Our mission is to promote a positive
+						and inclusive environment for Muslims on campus and to build bridges
+						of understanding and respect with other communities.
+					</p>
+				</div>
 			</div>
 			<div id="board" className="">
-				board
+				<h1>MSA Board</h1>
+				<p>
+					The Vanderbilt MSA is governed by a board of student leaders who are
+					elected annually by the membership. The board is responsible for
+					overseeing the organization's programs, events, and finances, as well
+					as representing the interests of the Muslim student community on
+					campus. If you are interested in joining the board or getting involved
+					with the Vanderbilt MSA, please contact us at [insert email address].
+					TODO: ADD CARDS FOR EACH BOARD MEMBER
+				</p>
 			</div>
 			<div id="prayerspaces" className="">
-				prayerspaces
+				<h1>Prayer Spaces on Campus</h1>
+				<p>
+					The Vanderbilt MSA provides access to several prayer spaces on campus,
+					including the Islamic Center of Nashville, the Multicultural Student
+					Life Center, and the Office of Religious Life. These spaces are
+					available for students to use for daily prayers, Jummah (Friday)
+					prayers, and special events. TODO: ADD CARDS FOR EACH PRAYER SPACE
+				</p>
 			</div>
 			<div id="otherorgs" className="">
-				otherorgs
+				<h1>Adjacent Orgs</h1>
+				<p>
+					The Vanderbilt MSA is proud to be part of a vibrant and supportive
+					community of organizations that share our mission to promote
+					diversity, inclusivity, and cultural exchange on campus. Our neighbors
+					include the Office of Religious Life, the Multicultural Student Life
+					Office, the Center for Student Wellbeing, and the Office of LGBTQI
+					Life, all of which provide a wide range of resources and programs
+					aimed at promoting equality and well-being for all students. TODO: ADD
+					CARDS FOR ORG
+				</p>
 			</div>
 			<div id="nashvillecom" className="">
-				nashvillecom
+				<h1>Nashville Community</h1>
+				<p>
+					Vanderbilt is located in the heart of Nashville, one of the most
+					vibrant and diverse cities in the country. The Nashville Muslim
+					community is an active and growing part of the city's cultural
+					landscape, offering a wide range of events, services, and
+					organizations for students to get involved with. If you are looking
+					for a mosque or Islamic center to attend, or simply want to explore
+					the local Muslim community, we encourage you to visit the Islamic
+					Center of Nashville's website or contact us for more information.
+					TODO: ADD CARDS FOR NASHVILLE COMMUNITY ORG
+				</p>
 			</div>
 			<div id="donate" className="">
-				donate
+				<h1>Donate</h1>
+				<p>
+					The Vanderbilt MSA is a student-run organization that relies on the
+					generosity of donors to support its programs and events. If you would
+					like to make a donation to the Vanderbilt MSA, please visit our
+					[insert website link] and select the "Donate" button. Your support is
+					greatly appreciated and will help us continue to serve the Muslim
+					student community at Vanderbilt.
+				</p>
 			</div>
 		</div>
 	);
--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1,39 +1,39 @@
 import "./globals.css";
 import NewCarousel from "./components/newCarousel";
 import hdate from "human-date";
+import MyGallery from "./components/newnewCarousel";
 // 'tw-elements'
 
 async function getPrayerTimes() {
-  const url = await `http://localhost:4000/prayerTimes`;
-  const res = await fetch(url, { cache: "no-store" });
-  const data = await res.json();
-  return data;
+	const url = await `http://localhost:4000/prayerTimes`;
+	const res = await fetch(url, { cache: "no-store" });
+	const data = await res.json();
+	return data;
 }
 async function getPhotoUrls() {
-  const url = await "http://localhost:4000/getPhotoLinks";
-  const res = await fetch(url, { cache: "no-store" });
-  const data = await res.json();
-  return data;
+	const url = await "http://localhost:4000/getPhotoLinks";
+	const res = await fetch(url, { cache: "no-store" });
+	const data = await res.json();
+	return data;
 }
 async function getEvents() {
-  const url = await "http://localhost:4000/getMSAEvents";
-  const res = await fetch(url, { cache: "no-store" });
-  const data = await res.json();
-  return data;
+	const url = await "http://localhost:4000/getMSAEvents";
+	const res = await fetch(url, { cache: "no-store" });
+	const data = await res.json();
+	return data;
 }
 
 // FIGURE OUT WHAT TO DO WITH CAROUSEL - AUTOMATIC OR BUTTONS/MANUAL
 // FIGURE OUT TIME CONVERSION
 export default async function Home() {
-  const times: any = await getPrayerTimes();
-  let photoUrls: any = await getPhotoUrls();
-  const calendarEvents: any = await getEvents();
-  
+	const times: any = await getPrayerTimes();
+	let photoUrls: any = await getPhotoUrls();
+	const calendarEvents: any = await getEvents();
 
-  const threeEvents: any = calendarEvents.events.slice(0, 1);
-  photoUrls = photoUrls.results.slice(0, 10);
+	const threeEvents: any = calendarEvents.events.slice(0, 1);
+	photoUrls = photoUrls.results.slice(0, 10);
 
-  /*
+	/*
     <div className="alert shadow-lg w-full items-center justify-center">
         <div>
           <svg
@@ -53,254 +53,256 @@ export default async function Home() {
         </div>
       </div>
   */
-  //
-  /*
+	//
+	/*
   {params.links.map((link: any, i: any) =>{
                         <div>
                             <img src={link} alt={"image"+i+1}/>
                         </div>
                 })}
   */
-  // JUMMAH UPDATE NEEDS TO BE ADDED
-  return (
-    <div>
-      <div className="flex flex-col gap-4 w-full align-center items-center justify-center">
-        <div
-          className="hero h-[50vh]"
-          style={{
-            backgroundImage: `url('https://www.commonapp.org/static/f14242e1e38d8f02ce26ed9f5e57c371/vanderbilt-university_277.jpg')`,
-          }}
-        >
-          <div className="hero-overlay bg-opacity-60"></div>
-          <div className="hero-content text-center text-neutral-content">
-            <div className="max-w-md">
-              <h1 className="mb-5 text-5xl font-bold">
-                Welcome to the Vanderbilt MSA!
-              </h1>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col lg:flex-row lg:h-full w-full gap-6">
-          <div className="flex flex-col gap-6 w-full items-center justify-center lg:h-fit lg:w-[50%] px-2 lg:rounded-lg"><PrayerTable iTimes={times} /></div>
-          <div className="flex flex-col lg:w-[50%] lg:h-full px-2 gap-6 w-full items-center justify-center pb-6">
-            <h1 className="font-bold text-3xl text-right">
-              The Next MSA Event!{" "}
-            </h1>
-            {threeEvents.map((event: any, i: any) => {
-              return (
-                <div key={i}>
-                  <Event className="h-full" event={event} />
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        
-        <div className="hidden lg:flex lg:align-center rounded-lg lg:items-center lg:justify-center lg:h-fit w-full px-2 mb-6 "><NewCarousel links={photoUrls}/></div>
-      </div>
-    </div>
-  );
+	// TODO: JUMMAH UPDATE NEEDS TO BE ADDED
+	// <MyGallery links={photoUrls}/>
+	return (
+		<div>
+			<div className="flex flex-col gap-4 w-full align-center items-center justify-center">
+				<div
+					className="hero h-[50vh]"
+					style={{
+						backgroundImage: `url('https://www.commonapp.org/static/f14242e1e38d8f02ce26ed9f5e57c371/vanderbilt-university_277.jpg')`,
+					}}
+				>
+					<div className="hero-overlay bg-opacity-60"></div>
+					<div className="hero-content text-center text-neutral-content">
+						<div className="max-w-md">
+							<h1 className="mb-5 text-5xl font-bold">
+								Welcome to the Vanderbilt MSA!
+							</h1>
+						</div>
+					</div>
+				</div>
+
+				<div className="flex flex-col lg:flex-row h-full w-full gap-6">
+					<div className="flex flex-col gap-6 w-full items-center justify-center lg:h-fit lg:w-[50%] px-2 lg:rounded-lg">
+						<PrayerTable iTimes={times} />
+					</div>
+					<div className="flex flex-col lg:w-[50%] lg:h-full px-2 gap-6 w-full items-center justify-center pb-6">
+						<h1 className="font-bold text-3xl text-right">
+							The Next MSA Event!{" "}
+						</h1>
+						{threeEvents.map((event: any, i: any) => {
+							return (
+								<div key={i}>
+									<Event className="h-full" event={event} />
+								</div>
+							);
+						})}
+					</div>
+				</div>
+
+				<div className="hidden lg:flex lg:align-center rounded-lg lg:items-center lg:justify-center lg:h-fit w-full px-2 mb-6 ">
+					<NewCarousel links={photoUrls} />
+				</div>
+			</div>
+		</div>
+	);
 }
+//
 
 // FIGURE OUT TIMEZONES
 // current Prayer
 function PrayerTable({ iTimes }: any) {
-  return (
-    <div className="flex flex-col align-center justify-center gap-4">
-      <h1 className="font-bold text-3xl text-center">Prayer Times</h1>
-      <h6 className="font-bold text-center">
-        {iTimes.IslamicDate.month} {iTimes.IslamicDate.day},{" "}
-        {iTimes.IslamicDate.year} {iTimes.currentTimeStamp}
-      </h6>
-      <table className="drop-shadow-lg table table-compact text-center border-seperate overflow-auto lg:w-full p-4">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Prayer Time:</th>
-            <th>Iqamah Time:</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Fajr:</td>
-            <td className={iTimes.currentPrayer === "fajr" ? "font-bold" : ""}>
-              {iTimes.prayerTimes.fajr === "undefined"
-                ? "Loading"
-                : iTimes.prayerTimes.fajr}
-            </td>
-            <td className={iTimes.currentPrayer === "fajr" ? "font-bold" : ""}>
-              {iTimes.iqammahTimes.fajrIqamah === "undefined"
-                ? "Loading"
-                : iTimes.iqammahTimes.fajrIqamah}
-            </td>
-          </tr>
-          <tr>
-            <td>Sunrise:</td>
-            <td
-              className={
-                iTimes.currentPrayer === "NoPrayerTime" ? "font-bold" : ""
-              }
-            >
-              {iTimes.prayerTimes.sunrise === "undefined"
-                ? "Loading"
-                : iTimes.prayerTimes.sunrise}
-            </td>
-            <td
-              className={
-                iTimes.currentPrayer === "NoPrayerTime" ? "font-bold" : ""
-              }
-            >
-              {iTimes.iqammahTimes.sunriseIqamah === "undefined"
-                ? "Loading"
-                : iTimes.iqammahTimes.sunriseIqamah}
-            </td>
-          </tr>
-          <tr>
-            <td>Zuhr:</td>
-            <td className={iTimes.currentPrayer === "zuhur" ? "font-bold" : ""}>
-              {iTimes.prayerTimes.zuhr === "undefined"
-                ? "Loading"
-                : iTimes.prayerTimes.zuhr}
-            </td>
-            <td className={iTimes.currentPrayer === "zuhur" ? "font-bold" : ""}>
-              {iTimes.iqammahTimes.zuhrIqamah === "undefined"
-                ? "Loading"
-                : iTimes.iqammahTimes.zuhrIqamah}
-            </td>
-          </tr>
-          <tr>
-            <td>Asr:</td>
-            <td className={iTimes.currentPrayer === "asr" ? "font-bold" : ""}>
-              {iTimes.prayerTimes.asr === "undefined"
-                ? "Loading"
-                : iTimes.prayerTimes.asr}
-            </td>
-            <td className={iTimes.currentPrayer === "asr" ? "font-bold" : ""}>
-              {iTimes.iqammahTimes.asrIqamah === "undefined"
-                ? "Loading"
-                : iTimes.iqammahTimes.asrIqamah}
-            </td>
-          </tr>
-          <tr>
-            <td>Maghrib:</td>
-            <td
-              className={iTimes.currentPrayer === "magrhib" ? "font-bold" : ""}
-            >
-              {iTimes.prayerTimes.maghrib === "undefined"
-                ? "Loading"
-                : iTimes.prayerTimes.maghrib}
-            </td>
-            <td
-              className={iTimes.currentPrayer === "magrhib" ? "font-bold" : ""}
-            >
-              {iTimes.maghribIqamah === "undefined" ? (
-                "Loading"
-              ) : (
-                <>{iTimes.maghribiqammah}</>
-              )}
-            </td>
-          </tr>
-          <tr>
-            <td>Isha: </td>
-            <td className={iTimes.currentPrayer === "isha" ? "font-bold" : ""}>
-              {iTimes.prayerTimes.isha === "undefined"
-                ? "Loading"
-                : iTimes.prayerTimes.isha}
-            </td>
-            <td className={iTimes.currentPrayer === "isha" ? "font-bold" : ""}>
-              {iTimes.iqammahTimes.ishaIqamah === "undefined"
-                ? "Loading"
-                : iTimes.iqammahTimes.ishaIqamah}
-            </td>
-          </tr>
-          <tr>
-            <td>First Jummah:</td>
-            <td>
-              {iTimes.iqammahTimes.firstJummahTime === "undefined"
-                ? "Loading"
-                : iTimes.iqammahTimes.firstJummahTime}
-            </td>
-            <td>N/A</td>
-          </tr>
-          <tr>
-            <td>Second Jummah:</td>
-            <td>
-              {iTimes.iqammahTimes.secondJummahTime === "undefined"
-                ? "Loading"
-                : iTimes.iqammahTimes.secondJummahTime}
-            </td>
-            <td>N/A</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div className="flex flex-col align-center justify-center gap-4">
+			<h1 className="font-bold text-3xl text-center">Prayer Times</h1>
+
+			<table className="drop-shadow-lg table table-compact text-center border-seperate overflow-auto lg:w-full p-4">
+				<thead>
+					<tr>
+						<th></th>
+						<th>Prayer Time:</th>
+						<th>Iqamah Time:</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Fajr:</td>
+						<td className={iTimes.currentPrayer === "fajr" ? "font-bold" : ""}>
+							{iTimes.prayerTimes.fajr === "undefined"
+								? "Loading"
+								: iTimes.prayerTimes.fajr}
+						</td>
+						<td className={iTimes.currentPrayer === "fajr" ? "font-bold" : ""}>
+							{iTimes.iqammahTimes.fajrIqamah === "undefined"
+								? "Loading"
+								: iTimes.iqammahTimes.fajrIqamah}
+						</td>
+					</tr>
+					<tr>
+						<td>Sunrise:</td>
+						<td
+							className={
+								iTimes.currentPrayer === "NoPrayerTime" ? "font-bold" : ""
+							}
+						>
+							{iTimes.prayerTimes.sunrise === "undefined"
+								? "Loading"
+								: iTimes.prayerTimes.sunrise}
+						</td>
+						<td
+							className={
+								iTimes.currentPrayer === "NoPrayerTime" ? "font-bold" : ""
+							}
+						>
+							{iTimes.iqammahTimes.sunriseIqamah === "undefined"
+								? "Loading"
+								: iTimes.iqammahTimes.sunriseIqamah}
+						</td>
+					</tr>
+					<tr>
+						<td>Zuhr:</td>
+						<td className={iTimes.currentPrayer === "zuhur" ? "font-bold" : ""}>
+							{iTimes.prayerTimes.zuhr === "undefined"
+								? "Loading"
+								: iTimes.prayerTimes.zuhr}
+						</td>
+						<td className={iTimes.currentPrayer === "zuhur" ? "font-bold" : ""}>
+							{iTimes.iqammahTimes.zuhrIqamah === "undefined"
+								? "Loading"
+								: iTimes.iqammahTimes.zuhrIqamah}
+						</td>
+					</tr>
+					<tr>
+						<td>Asr:</td>
+						<td className={iTimes.currentPrayer === "asr" ? "font-bold" : ""}>
+							{iTimes.prayerTimes.asr === "undefined"
+								? "Loading"
+								: iTimes.prayerTimes.asr}
+						</td>
+						<td className={iTimes.currentPrayer === "asr" ? "font-bold" : ""}>
+							{iTimes.iqammahTimes.asrIqamah === "undefined"
+								? "Loading"
+								: iTimes.iqammahTimes.asrIqamah}
+						</td>
+					</tr>
+					<tr>
+						<td>Maghrib:</td>
+						<td
+							className={iTimes.currentPrayer === "magrhib" ? "font-bold" : ""}
+						>
+							{iTimes.prayerTimes.maghrib === "undefined"
+								? "Loading"
+								: iTimes.prayerTimes.maghrib}
+						</td>
+						<td
+							className={iTimes.currentPrayer === "magrhib" ? "font-bold" : ""}
+						>
+							{iTimes.maghribIqamah === "undefined" ? (
+								"Loading"
+							) : (
+								<>{iTimes.maghribiqammah}</>
+							)}
+						</td>
+					</tr>
+					<tr>
+						<td>Isha: </td>
+						<td className={iTimes.currentPrayer === "isha" ? "font-bold" : ""}>
+							{iTimes.prayerTimes.isha === "undefined"
+								? "Loading"
+								: iTimes.prayerTimes.isha}
+						</td>
+						<td className={iTimes.currentPrayer === "isha" ? "font-bold" : ""}>
+							{iTimes.iqammahTimes.ishaIqamah === "undefined"
+								? "Loading"
+								: iTimes.iqammahTimes.ishaIqamah}
+						</td>
+					</tr>
+					<tr>
+						<td>First Jummah:</td>
+						<td>
+							{iTimes.iqammahTimes.firstJummahTime === "undefined"
+								? "Loading"
+								: iTimes.iqammahTimes.firstJummahTime}
+						</td>
+						<td>N/A</td>
+					</tr>
+					<tr>
+						<td>Second Jummah:</td>
+						<td>
+							{iTimes.iqammahTimes.secondJummahTime === "undefined"
+								? "Loading"
+								: iTimes.iqammahTimes.secondJummahTime}
+						</td>
+						<td>N/A</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 function Event({ event }: any) {
-  return (
-    <div>
-      <div className="card card-bordered glass bg-base-200 shadow-xl lg:h-full w-fit items-center justify-center">
-        <div className="card-body h-full">
-          {event.summary !== undefined ? (
-            <h2 className="card-title">{event.summary}</h2>
-          ) : (
-            <h2 className="card-title">(No Name)</h2>
-          )}
+	return (
+		<div className="card card-bordered glass bg-base-200 shadow-xl lg:h-full w-fit items-center justify-center">
+			<div className="card-body h-full">
+				{event.summary !== undefined ? (
+					<h2 className="card-title">{event.summary}</h2>
+				) : (
+					<h2 className="card-title">(No Name)</h2>
+				)}
 
-          {event.location !== undefined ? (
-            <h4>{event.location}</h4>
-          ) : (
-            <h4>Undefined/Online</h4>
-          )}
+				{event.location !== undefined ? (
+					<h4>{event.location}</h4>
+				) : (
+					<h4>Undefined/Online</h4>
+				)}
 
-          {event.start.timeZone !== undefined ? (
-            <h6>Timezone: {event.start.timeZone}</h6>
-          ) : (
-            <h6>(No Timezone)</h6>
-          )}
+				{event.start.timeZone !== undefined ? (
+					<h6>Timezone: {event.start.timeZone}</h6>
+				) : (
+					<h6>(No Timezone)</h6>
+				)}
 
-          {event.start.dateTime !== undefined ? (
-            <p>
-              Start Time:{" "}
-              {hdate.prettyPrint(new Date(event.start.dateTime), {
-                showTime: true,
-              })}
-            </p>
-          ) : (
-            <p></p>
-          )}
+				{event.start.dateTime !== undefined ? (
+					<p>
+						Start Time:{" "}
+						{hdate.prettyPrint(new Date(event.start.dateTime), {
+							showTime: true,
+						})}
+					</p>
+				) : (
+					<p></p>
+				)}
 
-          {event.end.dateTime !== undefined ? (
-            <p>
-              End Time:{" "}
-              {hdate.prettyPrint(new Date(event.end.dateTime), {
-                showTime: true,
-              })}
-            </p>
-          ) : (
-            <p></p>
-          )}
+				{event.end.dateTime !== undefined ? (
+					<p>
+						End Time:{" "}
+						{hdate.prettyPrint(new Date(event.end.dateTime), {
+							showTime: true,
+						})}
+					</p>
+				) : (
+					<p></p>
+				)}
 
-          {event.start.date !== undefined ? (
-            <p>Start Date: {hdate.prettyPrint(new Date(event.start.date))}</p>
-          ) : (
-            <p></p>
-          )}
+				{event.start.date !== undefined ? (
+					<p>Start Date: {hdate.prettyPrint(new Date(event.start.date))}</p>
+				) : (
+					<p></p>
+				)}
 
-          {event.end.date !== undefined ? (
-            <p>End Date: {hdate.prettyPrint(new Date(event.end.date))}</p>
-          ) : (
-            <p></p>
-          )}
+				{event.end.date !== undefined ? (
+					<p>End Date: {hdate.prettyPrint(new Date(event.end.date))}</p>
+				) : (
+					<p></p>
+				)}
 
-          {event.description !== undefined ? (
-            <p>{event.description}</p>
-          ) : (
-            <p>Undefined</p>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+				{event.description !== undefined ? (
+					<p>{event.description}</p>
+				) : (
+					<p>Undefined</p>
+				)}
+			</div>
+		</div>
+	);
 }
